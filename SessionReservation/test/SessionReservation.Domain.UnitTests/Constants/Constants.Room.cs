32da6ef077f81570198d37ad4792ef809c4025ee
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-03-05 21:42:48 +0200

    [ADD] gym cannot have more rooms than the subscription allows.

--- SessionReservation/test/SessionReservation.Domain.UnitTests/Constants/Constants.Room.cs
+++ SessionReservation/test/SessionReservation.Domain.UnitTests/Constants/Constants.Room.cs
@@ -6,5 +6,6 @@ public partial class Constants
     {
         public static Guid Id => Guid.CreateVersion7();
         public static int Capacity => 20;
+        public static int DailySessions => 3;
     }
 }
--- SessionReservation/test/SessionReservation.Domain.UnitTests/RoomAggregate/RoomTests.cs
+++ SessionReservation/test/SessionReservation.Domain.UnitTests/RoomAggregate/RoomTests.cs
@@ -1,4 +1,5 @@
-﻿using ErrorOr;
+﻿using System.Diagnostics;
+using ErrorOr;
 using SessionReservation.Domain.RoomAggregate;
 using SessionReservation.Domain.SessionAggregate;
 using SessionReservation.Domain.UnitTests.Common.Rooms;
@@ -20,5 +21,25 @@ public class RoomTests
 
         // Assert
         Assert.Equal(ErrorType.Forbidden, result.FirstError.Type);
+    }
+
+    [Fact]
+    public void ScheduleSession_CannotHaveMoreSessionThanTheSubscriptionAllows()
+    {
+        // Arrange
+        Room room = RoomFactory.CreateRoom(dailySessions: 1);
+        Session session1 = SessionFactory.CreateSession();
+        Session session2 = SessionFactory.CreateSession();
+
+        // Act
+        ErrorOr<Created> result1 = room.ScheduleSession(session1);
+        ErrorOr<Created> result2 = room.ScheduleSession(session2);
+
+        // Assert
+        Assert.Equal(Result.Created, result1.Value);
+        Assert.Equal(Result.Created, result2.Value);
+        Assert.Equal(ErrorType.Forbidden, result2.FirstError.Type);
+        
+        Assert.Equal(RoomErrors.RoomCannotHaveMoreSessionThanTheSubscriptionAllows, result2.FirstError);
     }
 }
