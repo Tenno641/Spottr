fead693abca0e58bd9a45182472ccd74aabbe4ad
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-04-02 07:21:02 +0200

    [ADD] Teach Session Method [ADD] Trainer Test Utilities [ADD] Trainer Errors

---
+++ SessionReservation/test/SessionReservation.Domain.UnitTests/TrainerAggregate/TrainerTests.cs
@@ -0,0 +1,31 @@
+﻿using ErrorOr;
+using SessionReservation.Domain.SessionAggregate;
+using SessionReservation.Domain.TrainerAggregate;
+using SessionReservation.Domain.UnitTests.Common.Sessions;
+using SessionReservation.Domain.UnitTests.Common.Trainers;
+
+namespace SessionReservation.Domain.UnitTests.TrainerAggregate;
+
+public class TrainerTests
+{
+    [Fact]
+    public void TeachSession_SessionsOverlap_ShouldFail()
+    {
+        // Arrange
+        Trainer trainer = TrainerFactory.Create();
+        Session session1 = SessionFactory.CreateSession();
+        Session session2 = SessionFactory.CreateSession();
+        
+        // Act
+        ErrorOr<Created> session1Result = trainer.TeachSession(session1);
+        ErrorOr<Created> session2Result =  trainer.TeachSession(session2);
+
+        // Assert
+        Assert.False(session1Result.IsError);
+        Assert.Equal(Result.Created, session1Result.Value);
+        
+        Assert.True(session2Result.IsError);
+        Assert.Equal(ErrorType.Conflict, session2Result.FirstError.Type);
+        Assert.Equal(TrainerErrors.CannotTeachOverlappingSessions, session2Result.FirstError);
+    }
+}
