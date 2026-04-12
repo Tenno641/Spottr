7e91f851d65fb90483e765ec586ccd2cbecb0e86
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-04-12 23:28:48 +0200

    [ADD] Room Removed Integration Event

--- SessionReservation/src/SessionReservation.Application/Rooms/IntegrationEvents/RoomDeletedIntegrationEventHandler.cs
+++ SessionReservation/src/SessionReservation.Application/Rooms/IntegrationEvents/RoomDeletedIntegrationEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using SessionReservation.Application.Common.Interfaces;
 using SessionReservation.Domain.RoomAggregate;
+using SessionReservation.Domain.SessionAggregate;
 using SharedKernel.GymManagement;
 
 namespace SessionReservation.Application.Rooms.IntegrationEvents;
@@ -8,18 +9,26 @@ namespace SessionReservation.Application.Rooms.IntegrationEvents;
 public class RoomDeletedIntegrationEventHandler: INotificationHandler<RoomDeletedIntegrationEvent>
 {
     private readonly IRoomsRepository _roomsRepository;
+    private readonly ISessionsRepository _sessionsRepository;
     
-    public RoomDeletedIntegrationEventHandler(IRoomsRepository roomsRepository)
+    public RoomDeletedIntegrationEventHandler(IRoomsRepository roomsRepository, ISessionsRepository sessionsRepository)
     {
         _roomsRepository = roomsRepository;
+        _sessionsRepository = sessionsRepository;
     }
 
     public async Task Handle(RoomDeletedIntegrationEvent notification, CancellationToken cancellationToken)
     {
         Room? room = await _roomsRepository.GetRoomByIdAsync(notification.RoomId);
-        if (room is null)
-            return;
-        
-        await _roomsRepository.DeleteRoomByIdAsync(room);
+
+        if (room is not null)
+        {
+            List<Session> sessions = await _sessionsRepository.ListBySessionsIdsAsync(room.SessionIds);
+
+            foreach (Session session in sessions)
+                room.CancelSession(session);
+            
+            await _roomsRepository.DeleteRoomByIdAsync(room);
+        }
     }
 }
